866e7434429e76a7403b34b41cdb3349d82ba3ad
Giovani Vitor <giovanileitevitor@gmail.com>
2020-05-05 09:46:15 -0300

    Convertido classe CardQuestionInterface to kotlin class

--- app/src/main/java/com/example/exemplocardview/componenteCardView/CardQuestionInterface.kt
+++ app/src/main/java/com/example/exemplocardview/componenteCardView/CardQuestionInterface.kt
@@ -1,13 +1,13 @@
 package com.example.exemplocardview.componenteCardView
 
-internal interface CardQuestionInterface {
+interface CardQuestionInterface {
     fun openCard()
     fun closeCard()
     fun isCardOpen(): Boolean
     fun isCardClose(): Boolean
-    fun textHeader(text : String?)
-    fun textQuestion(text: String?)
-    fun textRemarks(text: String?)
+    fun textHeader(text : String)
+    fun textQuestion(text: String)
+    fun textRemarks(text: String)
     fun setLikeIconActivated()
     fun setLikeIconDeactivated()
     fun setUnlikeIconActivated()
